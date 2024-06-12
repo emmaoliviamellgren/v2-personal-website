360648d561a1707ca611d0f0a188c05386b3da78
Emma <mellgrenemma0@gmail.com>
2024-06-12 20:52:30 +0200

    Initial commit

---
+++ app/_components/head.tsx
@@ -0,0 +1,10 @@
+const Head = () => {
+    return (
+    <div className="flex justify-center items-center flex-col gap-2 h-[20rem]">
+        <h1 className="">Emma Mellgren</h1>
+        <h3 className="text-secondary">Frontend Developer</h3>
+    </div>
+    );
+};
+
+export default Head;
